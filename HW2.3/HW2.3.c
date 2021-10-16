5ecf2b3dcf393c6a022502c879975a77b7fe3f93
Tausif Tamim <90731247+taus1ft@users.noreply.github.com>
2021-10-15 21:59:38 -0400

    Update HW2.3.c

--- HW2.3/HW2.3.c
+++ HW2.3/HW2.3.c
@@ -6,6 +6,6 @@ int main(void) {
     char value3;
     printf("Please provide a float value, an int value, and a character value with a space in between: \n");
     scanf("%f %i %c, ", &value1, &value2, &value3);
-    printf("Here is your returned values: \n %d, %f, %d", value2, value1, value3);
+    printf("Here is your returned values: \n %d, %f, %d \n", value2, value1, value3);
     return 0;
 }
